449bad9499a6e1895a312f57a9930df907a80be1
jinDeHao <o.idhmaid@gmail.com>
2024-03-22 08:41:51 +0000

    modify profile, reset_passwor, appointments

--- API/app/views/administration.py
+++ API/app/views/administration.py
@@ -3,7 +3,7 @@ from app.controllers.administration_controller import post_admin
 from flask import jsonify, abort
 
 
-@app_views.route("/setup_admin/<user_id>", methods=["PUT"])
+@app_views.route("/setup_admin/<user_id>", methods=["POST"])
 def setup_admin(user_id):
     """A route that the admin can use to set up a user for the admin role."""
     if user_id == "":
