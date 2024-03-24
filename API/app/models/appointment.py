939bc527af7bc5f528054c0ecf33123b30393604
jinDeHao <o.idhmaid@gmail.com>
2024-03-24 08:50:58 +0000

    done with workspaces, need to be checked

--- API/app/models/appointment.py
+++ API/app/models/appointment.py
@@ -10,8 +10,6 @@ class Appointment(BaseModel, Base):
     __tablename__ = "appointments"
     date = Column(DATETIME, default=datetime.now)
     range = Column(Integer)
-    status = Column(Enum("Pending", "Verified", "Canceled"), default="Pending")
-    attended = Column(Boolean, default=False)
-    verify = Column(Boolean, default=False)
+    status = Column(Enum("Pending", "Verified", "Canceled", "Attended", "Expired"), default="Pending")
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     workspace_id = Column(String(60), ForeignKey("workspaces.id"), nullable=False)
