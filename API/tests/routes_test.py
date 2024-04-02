f327e5c732024b471b27e37d1fe6e8f0ffed307e
jinDeHao <o.idhmaid@gmail.com>
2024-04-02 07:09:03 +0000

    session test

--- API/tests/routes_test.py
+++ API/tests/routes_test.py
@@ -21,11 +21,34 @@ class TestFlaskApp(unittest.TestCase):
                 "location": "Morocco Massa",
                 "picture": "none"}, follow_redirects=True)
         self.assertEqual(response.status_code, 201)
-        self.assertEqual('user created successfully', response.data.get('message'))
+        self.assertEqual("User signed up successfully", response.data.get('message'))
 
         """check user exists"""
         user = storage.session.query(User).filter_by(email="omaridhmaid@gmial.com").first()
         self.assertEqual(user.first_name, "omar")
+
+        """testing validation functionality"""
+        response = self.app.put(f'/api/validation/{user.token}')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual("account has been validated", response.data.get('message'))
+
+        """check user verify"""
+        self.assertEqual(user.verify, True)
+
+    def session_test(self):
+        """check log in"""
+        response = self.app.post('/api/login', data={
+                "email": "omaridhmaid@gmial.com",
+                "password": "omarIDH0.2"}, follow_redirects=True)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual("logged in", response.data.get('message'))
+
+
+        """check log out"""
+        response = self.app.delete('/api/logout')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual("logged out", response.data.get('message'))
+
 
 
 if __name__ == '__main__':
